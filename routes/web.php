52a2f149166bb13fce2596846056caef3ba8f1ed
amirjamshidii <amir.jamshidi@outlook.com>
2022-04-12 18:50:15 +0430

    second commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     $people = ['amir', 'saeed', 'reza'];
+
+//     return view('welcome', compact('people'));
+// });
+
+
+route::get('about', 'pagesController@home');
