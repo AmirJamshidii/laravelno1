491c26120416874d06985d1ba2f64ec5d276579c
amirjamshidii <amir.jamshidi@outlook.com>
2022-05-03 21:56:54 +0430

    whats going on?

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Card extends Model
 {
     use HasFactory;
+
+    public function notes() //اسم جدولی رو می‌ذاریم که می‌خوایم بهش مرتبط کنیم
+    {
+        return $this->hasMany(Note::class);
+    
+    }
 }
